b5fc0843cc4a73d0c524f0111410fa1776a7714e
Mucenieks <mucenieks@gmail.com>
2016-01-07 14:17:55 +0200

    Loudablelayout Challange - adjusted for tutorial result

--- LaudableLayoutChallange/App.xaml.cs
+++ LaudableLayoutChallange/App.xaml.cs
@@ -43,7 +43,7 @@ namespace LaudableLayoutChallange
 #if DEBUG
             if (System.Diagnostics.Debugger.IsAttached)
             {
-                this.DebugSettings.EnableFrameRateCounter = true;
+                //this.DebugSettings.EnableFrameRateCounter = true; //This removes or sets frame counter on debug view
             }
 #endif
 
